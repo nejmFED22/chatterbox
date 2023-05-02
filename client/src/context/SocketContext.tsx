1f9109bcd846308cadea728ed0c2c94915da7807
Ellen Dahlgren <ellensofiadahlgren@outlook.com>
2023-05-02 14:25:30 +0200

    initial commit

---
+++ client/src/context/SocketContext.tsx
@@ -0,0 +1,55 @@
+import {
+  PropsWithChildren,
+  createContext,
+  useContext,
+  useEffect,
+  useState,
+} from "react";
+import { Socket, io } from "socket.io-client";
+import {
+  ClientToServerEvents,
+  ServerToClientEvents,
+} from "../../../server/communication";
+
+interface ContextValues {
+  socket: Socket;
+}
+
+const SocketContext = createContext<ContextValues>(null as any);
+export const useSocket = () => useContext(SocketContext);
+export const socket: Socket<ServerToClientEvents, ClientToServerEvents> = io();
+
+function SocketProvider({ children }: PropsWithChildren) {
+  const [socket] =
+    useState<Socket<ServerToClientEvents, ClientToServerEvents>>(io);
+
+  useEffect(() => {
+    function connect() {
+      console.log("Connected to server");
+    }
+    function disconnect() {
+      console.log("Disonnected from server");
+    }
+    function message(message: string) {
+      console.log(message);
+    }
+
+    socket.on("connect", connect);
+    socket.on("message", message);
+    socket.on("disconnect", disconnect);
+
+    return () => {
+      socket.on("connect", connect);
+      socket.off("message", message);
+      socket.off("disconnect", disconnect);
+    };
+  }, [socket]);
+
+  return (
+    <SocketContext.Provider value={{ socket }}>
+      {children}
+    </SocketContext.Provider>
+  );
+}
+
+export default SocketProvider;
---
+++ server/server.ts
@@ -0,0 +1,21 @@
+import { Server } from "socket.io";
+import {
+  ClientToServerEvents,
+  InterServerEvents,
+  ServerToClientEvents,
+  SocketData,
+} from "./communication";
+
+const io = new Server<
+  ClientToServerEvents,
+  ServerToClientEvents,
+  InterServerEvents,
+  SocketData
+>();
+
+io.on("connection", (socket) => {
+  console.log("a user connected");
+});
+
+io.listen(3000);
+console.log("listening on port 3000");
