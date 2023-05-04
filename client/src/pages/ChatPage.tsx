55a193a5229668fa7bd59040f6bf778c3c7b0b7d
NathanaelBlackbourn <nathanaelblackbourn@gmail.com>
2023-05-04 09:45:30 +0200

    Pushing project structure to group

--- client/src/pages/ChatPage.tsx
+++ client/src/pages/ChatPage.tsx
@@ -1,19 +1,27 @@
 import { Box, Container } from "@mui/material";
-import { CSSProperties, Fragment } from "react";
+import { CSSProperties, Fragment, useEffect, useRef, useState } from "react";
 import Header from "../components/Header";
 import MessageInput from "../components/MessageInput";
 import MessageStack from "../components/MessageStack";
 import Sidebar from "../components/Sidebar";
 
 export default function ChatPage() {
+  const [inputHeight, setInputHeight] = useState(0);
+
+  const inputRef = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    inputRef.current && setInputHeight(inputRef.current.clientHeight);
+  }, [inputRef]);
+
   return (
     <Fragment>
       <Box sx={styledBox} component={"main"}>
         <Header />
-        <Container>
+        <Container sx={{ marginBottom: inputHeight }}>
           <MessageStack />
         </Container>
-        <Container sx={styledInputContainer}>
+        <Container component={"div"} sx={styledInputContainer} ref={inputRef}>
           <MessageInput />
         </Container>
       </Box>
