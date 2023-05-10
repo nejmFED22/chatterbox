ecbc3e3b7a03717c0f4d703ca6dd7bf0d5ab306a
Jenny Pettersson <j.m.pettersson@hotmail.se>
2023-05-10 19:17:16 +0200

    some styling

--- client/src/components/sidebar-components/SidebarUserList.tsx
+++ client/src/components/sidebar-components/SidebarUserList.tsx
@@ -1,5 +1,6 @@
-import { List, ListItem } from "@mui/material";
+import { Link, List, ListItem, Typography } from "@mui/material";
 import { useSocket } from "../../context/SocketContext";
+import { theme } from "../../theme";
 
 export default function SidebarUserList() {
   const { userList } = useSocket();
@@ -7,8 +8,31 @@ export default function SidebarUserList() {
   return (
     <List>
       {userList.map((user) => (
-        <ListItem key={user.userID}>{user.username}</ListItem>
+        <ListItem key={user.userID} sx={listItemStyling}>
+          <Link sx={styledLink}>
+          <Typography variant="h4">{user.username}</Typography>          
+          </Link>
+        </ListItem>
       ))}
     </List>
   );
 }
+
+const styledLink = {
+  padding: "1rem 2rem",
+  textDecoration: "none",
+  width: "100%",
+  weight: "100%",
+  color: theme.palette.primary.dark,
+  "&:hover": {
+    backgroundColor: theme.palette.primary.dark,
+    color: theme.palette.primary.light,
+    textDecoration: "underline",
+  },
+}
+
+const listItemStyling = {
+  padding: 0,
+}
+
+
