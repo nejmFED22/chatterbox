f4d79198f19d98a9af3cfe0c185f07967faea65f
NathanaelBlackbourn <nathanaelblackbourn@gmail.com>
2023-05-11 11:20:15 +0200

    Lines added.

--- server/server.ts
+++ server/server.ts
@@ -71,6 +71,14 @@ const main = async () => {
     next();
   });
 
+  io.of("/").adapter.on("create-room", (room) => {
+    console.log(`room ${room} was created`);
+  });
+
+  io.of("/").adapter.on("join-room", (room, id) => {
+    console.log(`socket ${id} has joined room ${room}`);
+  });
+
   io.on("connection", async (socket) => {
     // Setup for client
     console.log("A user has connected");
